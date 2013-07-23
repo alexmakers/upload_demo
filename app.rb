47a712a04936b18e85f7ac590fda8406f4271e04
Alex Peattie <alexpeattie@gmail.com>
2013-07-23 10:06:57 +0100

    Upload to S3 functionality

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
 require 'sinatra'
+require 'aws/s3'
 
-get '/form' do
+get '/' do
   erb :form
 end
 
 post '/upload' do
-  params.inspect
+  # path = "uploads/" + params[:upload][:filename]
+  #   
+  #   IO.write(path, params[:upload][:tempfile].read)
+  
+  AWS::S3::Base.establish_connection!(
+    :access_key_id     => 'AKIAJPD4QQS6C5BNQF5Q',
+    :secret_access_key => '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'
+  )
+  AWS::S3::S3Object.store(
+    params[:upload][:filename],
+    params[:upload][:tempfile].read,
+    'makers-upload-demo'
+  )
 end
